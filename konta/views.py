4bff1ec4a18a47910050a770227ce85f739f0756
czumper <kfranek44@gmail.com>
2025-02-27 00:09:53 +0100

    hshshsh

--- konta/views.py
+++ konta/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserProfileForm
 from django.contrib.auth import logout
+from .models import UserProfile
 
 @login_required
 def edytuj_profil(request):

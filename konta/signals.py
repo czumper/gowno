ebc633139a9ed4c0faa2622bba7c289730b2994f
czumper <kfranek44@gmail.com>
2025-03-01 18:56:21 +0100

    ujddojijdsiojio

--- konta/signals.py
+++ konta/signals.py
@@ -3,6 +3,7 @@ from django.contrib.auth.signals import user_logged_in
 from django.core.mail import send_mail
 from allauth.account.signals import password_changed
 from django.conf import settings
+from django.contrib import messages
 
 @receiver(password_changed)
 def send_password_change_confirmation(sender, request, user, **kwargs):
@@ -11,3 +12,4 @@ def send_password_change_confirmation(sender, request, user, **kwargs):
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [user.email]
     send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+    messages.success(request, "Hasło zmienione pomyślnie!", extra_tags="password_changed")
